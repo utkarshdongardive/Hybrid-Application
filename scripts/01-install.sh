b03bdc0c484a2443604c9b6bed0ed7b3d4e5f77f
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-07-06 23:03:10 +0530

    changes

---
+++ scripts/01-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo 'installation started .............................'
+
+cd ../src
+
+cd 01-prereq
+oc apply -f 01-namespace.yaml
+
+cd ../02-channel
+oc apply -f 11-channel.yaml
+
+cd ../03-subscription
+oc apply -f 11-application.yaml
+oc apply -f 21-placement.yaml
+oc apply -f 31-subscription.yaml
+
+echo 'installation completed .............................'
---
+++ scripts/02-uninstall.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo 'un-installation started .............................'
+
+cd ../src
+
+cd 03-subscription
+oc delete -f 31-subscription.yaml
+oc delete -f 21-placement.yaml 
+oc delete -f 11-application.yaml
+
+cd ../02-channel
+oc delete -f 11-channel.yaml 
+
+cd ../01-prereq
+oc delete -f 01-namespace.yaml 
+
+echo 'un-installation completed .............................'
